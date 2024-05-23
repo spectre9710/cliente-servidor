e3f188f3b5c6395c101abd1419fe8f08ed0b0650
spectre9710 <ramialan176@gmail.com>
2024-05-23 08:45:31 -0600

    front

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import img1 from "./img/Imagen1.jpeg";
+import img2 from "./img/Imagen2.jpg";
+import img3 from "./img/Imagen3.jpg";
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: img1,
+            title: "Coca Cola",
+            desc: "",
+            price: 50,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: "Doritos",
+            desc: "",
+            price: 18,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: "Mantecadas",
+            desc: "",
+            price: 20,
+        },
+    ],
+};
+
+export default data;
